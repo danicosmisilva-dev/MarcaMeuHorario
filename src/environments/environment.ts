8d59392596bf30007a4df0c57961cad4ac1219b5
Dani Cosmi <danicosmisilva@gmail.com>
2025-11-16 20:33:29 -0300

    Add firebase SDK

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBSnpQLwAv7CwEpDWkLuLiMhYfiQu6-i1I",
+    authDomain: "marca-meu-horario.firebaseapp.com",
+    projectId: "marca-meu-horario",
+    storageBucket: "marca-meu-horario.appspot.com",
+    messagingSenderId: "715616776368",
+    appId: "1:715616776368:web:5936d09a035f863a90b75",
+    measurementId: "G-QHXYVPKNYZ",
+  }
+};
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,17 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+import { initializeApp } from "firebase/app";
+
+import { getAnalytics } from "firebase/analytics";
+
+import { environment } from './environments/environment';
+
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+// Initialize Firebase
+
+const app = initializeApp(environment.firebase);
+
+const analytics = getAnalytics(app);
