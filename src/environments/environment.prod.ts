5274c131795ae98436094add682ae74b2d85f609
Dani Cosmi <danicosmisilva@gmail.com>
2025-11-16 20:33:29 -0300

    Add firebase SDK

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "FIREBASE_API_KEY_REMOVED",
+    authDomain: "marca-meu-horario.firebaseapp.com",
+    projectId: "marca-meu-horario",
+    storageBucket: "marca-meu-horario.appspot.com",
+    messagingSenderId: "FIREBASE_ID_REMOVED",
+    appId: "1:FIREBASE_ID_REMOVED:web:5936d09a035f863a90b75",
+    measurementId: "MEASUREMENT_ID_REMOVED",
+  }
+};
